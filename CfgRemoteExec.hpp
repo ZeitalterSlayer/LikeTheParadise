f293f664e885f1db2cfad55061b8e04b43b9fcc5
Slayer <hendrik.weber@youngnews.org>
2016-07-06 19:42:38 +0200

    Rausgenommen

--- CfgRemoteExec.hpp
+++ CfgRemoteExec.hpp
@@ -164,7 +164,6 @@ class CfgRemoteExec {
         F(life_fnc_setFuel,ANYONE)
         F(life_fnc_simDisable,ANYONE)
         F(SPY_fnc_notifyAdmins,ANYONE)
-        F(TON_fnc_handleBlastingCharge,ANYONE)
     };
 
     class Commands {
