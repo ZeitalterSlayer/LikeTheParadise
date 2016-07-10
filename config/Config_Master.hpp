1e5f15adb21603448e2972e286f982baf93bb0c6
I2azer <jaxhoizi@gmail.com>
2016-07-10 18:41:10 +0200

    More

--- config/Config_Master.hpp
+++ config/Config_Master.hpp
@@ -143,7 +143,7 @@ class Life_Settings {
 
     /* "Other" Vehicle Prices */
     vehicle_chopShop_multiplier = .25; //Chop Shop price for vehicles. TO AVOID EXPLOITS NEVER SET HIGHER THAN A PURCHASE/RENTAL multipler!   Payout = Config_vehicle Price * multiplier
-    vehicle_storage_fee_multiplier = .04; //Pull from garage cost --> Cost takes the playersides Buy Price * multiplier
+    vehicle_storage_fee_multiplier = .03; //Pull from garage cost --> Cost takes the playersides Buy Price * multiplier
     vehicle_cop_impound_multiplier = .05; //TO AVOID EXPLOITS NEVER SET HIGHER THAN A PURCHASE/RENTAL multipler!   Payout = Config_vehicle Price * multiplier
 
     /* Wanted System Settings *
--- config/Config_Weapons.hpp
+++ config/Config_Weapons.hpp
@@ -25,7 +25,6 @@ class WeaponShops {
             { "SMG_02_F", "",40000, -1 },
             { "SMG_01_F", "",40000, -1 },
             { "hgun_PDW2000_F", "",25000, -1 },
-			{ "SMG_05_F", "", 20000, -1 },
             { "hgun_Rook40_F", "",15000, -1 },
             { "hgun_Pistol_heavy_02_F", "",15000, -1 },
             { "hgun_Pistol_heavy_01_F", "",15000, -1 },
@@ -47,10 +46,7 @@ class WeaponShops {
 			{ "optic_MRD", "",2500 },
             { "optic_Yorris", "",2500 },
             { "optic_Aco_smg", "",2500 },
-            { "optic_ACO_grn_smg", "",2500 },
-            { "optic_Holosight_smg", "",2500 },
-            { "muzzle_snds_L", "",30000 },
-            { "muzzle_snds_acp", "",30000 }
+            { "optic_ACO_grn_smg", "",2500 }
         };
     };
 
