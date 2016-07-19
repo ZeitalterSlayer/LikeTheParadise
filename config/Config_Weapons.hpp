5f1fc4d1816e67a0aa5e1056fa9473add053de70
I2azer <jaxhoizi@gmail.com>
2016-07-19 15:43:14 +0200

    undo

--- config/Config_Weapons.hpp
+++ config/Config_Weapons.hpp
@@ -1006,7 +1006,7 @@ class rebelthree {
         name = "Tanoa Assistant Chief Shop";
         side = "cop";
         license = "";
-        item[] = {
+        items[] = {
             { "hgun_P07_snds_F", "Taser", 2000, { "life_coplevel", "SCALAR", 1 } },			//Taser
 			{ "arifle_sdar_F", "Taser Rifle", 5000, { "life_coplevel", "SCALAR", 2 } },		//Taser Rifle
             { "SMG_05_F", "", 2000, { "life_coplevel", "SCALAR", 2 } },                       //Protector
