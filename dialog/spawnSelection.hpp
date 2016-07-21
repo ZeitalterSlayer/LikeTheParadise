5b5ada21245ee1b871c65e76927e49e7032136cd
Slayer <hendrik.weber@youngnews.org>
2016-07-21 16:49:52 +0200

    Revert "NewSpawn 2"
    This reverts commit 3b14be0cb99a97c8570d7d9bd77a82f07df232ba.

--- dialog/spawnSelection.hpp
+++ dialog/spawnSelection.hpp
@@ -7,72 +7,60 @@ class life_spawn_selection {
     enableSimulation = 1;
 
     class controlsBackground {
-        class BGha: Life_RscPicture {
-            idc = 38531;
-            text = "textures\hintergrund.jpg";
-            x = 0 * safezoneW + safezoneX;
-            y = 0 * safezoneH + safezoneY;
-            w = 1 * safezoneW;
-            h = 1 * safezoneH;
-    };
-
         class life_RscTitleBackground: Life_RscText
         {
             idc = -1;
             text = "";
-            x = 0.317578 * safezoneW + safezoneX;
-            y = 0.322684 * safezoneH + safezoneY;
-            w = 0.353906 * safezoneW;
-            h = 0.375 * safezoneH;
+            x = 0.246093 * safezoneW + safezoneX;
+            y = 0.225462 * safezoneH + safezoneY;
+            w = 0.498437 * safezoneW;
+            h = 0.569444 * safezoneH;
             ColorBackground[] = {0,0,0,0.694117};
             ColorText[] = {1,1,1,1};
         };
     };
-
     class controls {
         class MapView: Life_RscMapControl
         {
-            idc = 38502;
-            x = 0.460156 * safezoneW + safezoneX;
-            y = 0.356712 * safezoneH + safezoneY;
-            w = 0.196875 * safezoneW;
-            h = 0.306944 * safezoneH;
+            idc = 38501;
+            x = 0.474804 * safezoneW + safezoneX;
+            y = 0.263136 * safezoneH + safezoneY;
+            w = 0.245703 * safezoneW;
+            h = 0.477430 * safezoneH;
             maxSatelliteAlpha = 0.75;
             alphaFadeStartScale = 1.15;
             alphaFadeEndScale = 1.29;
         };
-
-        class spawnButton: Life_RscButtonMenu
-        {
-            idc = -1;
-            text = "Spawn";
-            onButtonClick = "[] call life_fnc_spawnConfirm";
-            x = 0.339453 * safezoneW + safezoneX;
-            y = 0.610184 * safezoneH + safezoneY;
-            w = 0.114453 * safezoneW;
-            h = 0.055555 * safezoneH;
-            ColorBackground[] = {1,0,0,1};
-            ColorText[] = {1,1,1,1};
-            font = TahomaB;
-            SizeEx = 0.4;
-            colorFocused[] = {0,0,0,1};
-            colorDisabled[] = {0,0,0,1};
-        };
-
-        class SpawnPointList: Life_RscListNBox
+        class SpawnPointList: Life_RscListbox
         {
             idc = 38510;
-            x = 0.340234 * safezoneW + safezoneX;
-            y = 0.357233 * safezoneH + safezoneY;
-            w = 0.113671 * safezoneW;
-            h = 0.250347 * safezoneH;
-            ColorBackground[] = {0,0,0,0.705882};
+            x = 0.272656 * safezoneW + safezoneX;
+            y = 0.266781 * safezoneH + safezoneY;
+            w = 0.192187 * safezoneW;
+            h = 0.421180 * safezoneH;
+            onLBSelChanged = "_this call life_fnc_spawnPointSelected;";
+            ColorBackground[] = {0,0,0,1};
             ColorText[] = {1,1,1,1};
             font = TahomaB;
             SizeEx = 0.4;
             colorSelectBackground[] = {0,0,0,1};
             colorSelect[] = {0,0,0,1};
-            onLBSelChanged = "_this call life_fnc_spawnPointSelected;";
+        };
+        class spawnButton: Life_RscButtonMenu
+        {
+            idc = -1;
+            text = "Spawnen";
+            onButtonClick = "[] call life_fnc_spawnConfirm";
+            x = 0.271093 * safezoneW + safezoneX;
+            y = 0.693518 * safezoneH + safezoneY;
+            w = 0.19375 * safezoneW;
+            h = 0.055555 * safezoneH;
+            ColorBackground[] = {1,0,0,1};
+            ColorText[] = {0.941176,1,1,1};
+            font = TahomaB;
+            SizeEx = 0.4;
+            colorFocused[] = {0,0,0,1};
+            colorDisabled[] = {0,0,0,1};
         };
     };
 };
