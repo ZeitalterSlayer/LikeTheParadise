50a33e5b4984d699023394595d045a6a4dfb628e
Slayer <hendrik.weber@youngnews.org>
2016-07-21 16:40:12 +0200

    Altes Spawnmenü umgestellt

--- dialog/spawnSelection.hpp
+++ dialog/spawnSelection.hpp
@@ -1,12 +1,10 @@
-////////////////////////////////////////////////////////////////////////////
-//////           This HPP was created by Shinji`s GUI tool            //////
-////////////////////////////////////////////////////////////////////////////
 class life_spawn_selection {
     idd = 38500;
     movingEnable = 0;
     enableSimulation = 1;
 
     class controlsBackground {
+
         class BGha: Life_RscPicture {
             idc = 38531;
             text = "textures\hintergrund.jpg";
@@ -16,64 +14,78 @@ class life_spawn_selection {
             h = 1 * safezoneH;
     };
 
-        class life_RscTitleBackground: Life_RscText
-        {
+        class life_RscTitleBackground: Life_RscText    {
+            colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", "(profilenamespace getvariable ['GUI_BCG_RGB_A',0.7])"};
+            idc = -1;
+            x = 0.1;
+            y = 0.2;
+            w = 0.8;
+            h = (1 / 25);
+        };
+
+        class MainBackground: Life_RscText {
+            colorBackground[] = {0,0,0,0.7};
+            idc = -1;
+            x = 0.1;
+            y = 0.2 + (11 / 250);
+            w = 0.8;
+            h = 0.6 - (22 / 250);
+        };
+
+        class Title: Life_RscTitle {
+            colorBackground[] = {0,0,0,0};
             idc = -1;
             text = "";
-            x = 0.317578 * safezoneW + safezoneX;
-            y = 0.322684 * safezoneH + safezoneY;
-            w = 0.353906 * safezoneW;
-            h = 0.375 * safezoneH;
-            ColorBackground[] = {0,0,0,0.694117};
-            ColorText[] = {1,1,1,1};
+            x = 0.1;
+            y = 0.2;
+            w = 0.8;
+            h = (1 / 25);
+        };
+
+        class SpawnPointTitle: Title {
+            idc = 38501;
+            style = 1;
+            text = "";
+        };
+
+        class MapView : Life_RscMapControl {
+            idc = 38502;
+            x = 0.328;
+            y = 0.26;
+            w = 0.56;
+            h = 0.56 - (22 / 250);
+            maxSatelliteAlpha = 0.75;//0.75;
+            alphaFadeStartScale = 1.15;//0.15;
+            alphaFadeEndScale = 1.29;//0.29;
         };
     };
 
     class controls {
-        class MapView: Life_RscMapControl
-        {
-            idc = 38502;
-            x = 0.460156 * safezoneW + safezoneX;
-            y = 0.356712 * safezoneH + safezoneY;
-            w = 0.196875 * safezoneW;
-            h = 0.306944 * safezoneH;
-            maxSatelliteAlpha = 0.75;
-            alphaFadeStartScale = 1.15;
-            alphaFadeEndScale = 1.29;
-        };
-
-        class spawnButton: Life_RscButtonMenu
-        {
-            idc = -1;
-            text = "Spawn";
-            onButtonClick = "[] call life_fnc_spawnConfirm";
-            x = 0.339453 * safezoneW + safezoneX;
-            y = 0.610184 * safezoneH + safezoneY;
-            w = 0.114453 * safezoneW;
-            h = 0.055555 * safezoneH;
-            ColorBackground[] = {1,0,0,1};
-            ColorText[] = {1,1,1,1};
-            font = TahomaB;
-            SizeEx = 0.4;
-            colorFocused[] = {0,0,0,1};
-            colorDisabled[] = {0,0,0,1};
-        };
-
-        class SpawnPointList: Life_RscListNBox
-        {
+        class SpawnPointList: Life_RscListNBox {
             idc = 38510;
-            x = 0.340234 * safezoneW + safezoneX;
-            y = 0.357233 * safezoneH + safezoneY;
-            w = 0.113671 * safezoneW;
-            h = 0.250347 * safezoneH;
-            ColorBackground[] = {0,0,0,0.705882};
-            ColorText[] = {1,1,1,1};
-            font = TahomaB;
-            SizeEx = 0.4;
-            colorSelectBackground[] = {0,0,0,1};
-            colorSelect[] = {0,0,0,1};
+            text = "";
+            sizeEx = 0.041;
+            coloumns[] = {0,0,0.9};
+            drawSideArrows = 0;
+            idcLeft = -1;
+            idcRight = -1;
+            rowHeight = 0.050;
+            x = 0.105;
+            y = 0.26;
+            w = (8.8 / 40);
+            h = (10 / 25);
             onLBSelChanged = "_this call life_fnc_spawnPointSelected;";
+        };
+
+        class spawnButton: Life_RscButtonMenu {
+            idc = -1;
+            colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", 0.5};
+            text = "$STR_Spawn_Spawn";
+            onButtonClick = "[] call life_fnc_spawnConfirm";
+            x = 0.11;
+            y = 0.69;
+            w = (8 / 40);
+            h = (1 / 25);
         };
     };
 };
-////////////////////////////////////////////////////////////////////////////
