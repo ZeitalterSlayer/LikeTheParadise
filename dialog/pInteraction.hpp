c37bf9c9b58eec4a5e018bb6a2ab7d2efc6a1af3
Slayer <hendrik.weber@youngnews.org>
2016-07-22 11:52:24 +0200

    Revert "Test 123"
    This reverts commit 1b4002c6e3d7b16dd260a43805775c365a94791b.

--- dialog/pInteraction.hpp
+++ dialog/pInteraction.hpp
@@ -145,7 +145,6 @@ class vInteraction_Menu {
     enableSimulation = 1;
 
     class controlsBackground {
-      /*
         class RscTitleBackground: life_RscPicture {
             idc = -1;
             text= "textures\pinter.paa"
@@ -155,14 +154,13 @@ class vInteraction_Menu {
             h = (1 / 25);
         };
 
-        */
         class MainBackground: life_RscPicture {
             idc = -1;
             text= "textures\pinter.paa"
-            x = BGX;
-            y = BGY;
-            w = BGW + 0.5;
-            h = (1 / 25);
+            x = 0.335 * safezoneW + safezoneX;
+            y = 0.203 * safezoneH + safezoneY;
+            w = 0.43 * safezoneW;
+            h = 0.605 * safezoneH;
         };
 
         class Title: life_RscTitle {
