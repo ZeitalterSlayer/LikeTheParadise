1290568ea1e72a57c66b864f52f74a91d75aeb46
LikeTheParadise-POZI <liketheparadise.pozi@gmail.com>
2016-07-23 15:52:14 +0200

    Öl Fix?

--- config/Config_Gather.hpp
+++ config/Config_Gather.hpp
@@ -164,7 +164,7 @@ Example 2:
             zones[] = { "oil_field_1", "oil_field_2" };
             item = "pickaxe";
             mined[] = { "oil_unprocessed" };
-			zoneSize = 100;
+			zoneSize = 1000;
         };
     };
 };
